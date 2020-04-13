577c32d157bf12ceb08cb4a53722cae425c43b90
Yan Kaique <yankaiquecosta.yk@gmail.com>
2020-04-13 14:44:42 -0300

    Props in VueJS: $emit, $on, Event Bus and Validation

---
+++ 8. Comunicação Entre Componentes/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>Nome é <strong>{{nome}}</strong></p>
+        <p>Idade é <strong>{{idade}}</strong> </p>
+        <button @click="alterarNome">Alterar nome</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info 
+                :nome='nome'
+                :idade='idade'
+                @nomeMudou="nome = $event.novo"
+                :reiniciarfn='reiniciarNome'
+                
+                />
+            <app-usuario-editar :idade="idade"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data(){
+        return{
+            nome:'Pedro',
+            idade:21
+        }
+    },
+    methods:{
+        alterarNome(){
+            return this.nome = 'Ana';
+        },
+        reiniciarNome(){
+            this.nome = 'Pedro'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ 8. Comunicação Entre Componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do Usuário:
+      <strong>{{inverterNome()}}</strong>
+    </p>
+    <p>
+      Idade do Usuáro
+      <strong>{{idade}}</strong>
+    </p>
+    <button @click="reiniciarNome">Reiniciar nome</button>
+    <button @click="reiniciarfn">Reiniciar Nome (Callback)</button>
+  </div>
+</template>
+
+<script>
+import barramento from "@/barramento";
+
+export default {
+  props: {
+    nome: {
+      type: String,
+      // require: true,
+      // default: function(){
+      //     return Array(10).fill(0).join(',')
+      // }
+      default: "Anônimo"
+    },
+    reiniciarfn: Function,
+    idade: Number
+  },
+  methods: {
+    inverterNome() {
+      return this.nome
+        .split("")
+        .reverse()
+        .join("");
+    },
+    reiniciarNome() {
+      const antigo = this.nome;
+      this.nome = "Pedro";
+      this.$emit("nomeMudou", {
+        novo: this.nome,
+        antigo
+      });
+    }
+  },
+  created() {
+    barramento.quandoIdadeMudar(idade => {
+      this.idade = idade;
+    });
+  }
+};
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
